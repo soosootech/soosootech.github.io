9cf3905976f0ed4218c887d1ff50f069e657d895
Dongsu Jang <77693+iolo@users.noreply.github.com>
2024-07-30 12:38:17 +0900

    add index page with minimal styles and theme switcher

---
+++ script.js
@@ -0,0 +1,30 @@
+(function (document, localStorage) {
+  const root = document.documentElement;
+  const themeCheck = document.getElementById('themeCheck');
+
+  function setTheme(theme) {
+    switch (theme) {
+      case 'light':
+        root.classList.remove('dark-theme');
+        root.classList.add('light-theme');
+        root.style.setProperty('--bg-color', '#ffffff');
+        root.style.setProperty('--text-color', '#333333');
+        break;
+      case 'dark':
+        root.classList.remove('light-theme');
+        root.classList.add('dark-theme');
+        root.style.setProperty('--bg-color', '#333333');
+        root.style.setProperty('--text-color', '#ffffff');
+        break;
+        // use system theme
+        break;
+    }
+    localStorage.setItem('theme', theme);
+  }
+  themeCheck.addEventListener('change', (e) => setTheme(e.target.checked ? 'dark' : 'light'));
+
+  const systemTheme = window.matchMedia('(prefers-color-scheme: dark)') ? 'dark' : 'light';
+  const savedTheme = localStorage.getItem('theme') || systemTheme;
+  themeCheck.checked = savedTheme;
+  setTheme(savedTheme);
+})(document, localStorage);
